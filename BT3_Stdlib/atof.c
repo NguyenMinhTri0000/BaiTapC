45d1848a3c10f1148e47e9a3093f695ef21335ca
NMT <nguyenminhtri.20122001@gmail.com>
2022-07-23 13:15:23 +0700

    them itoa, atof co y tuong nhung chua duoc

---
+++ BT3_Stdlib/atof.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int Atof(char chuoi[])
+{
+    int i = 0;
+    int val = 0;
+    int point;
+    while (chuoi[i] != '\0')
+    {
+        if(chuoi[i]>=48 && chuoi[i]<=57)    //từ 0 đến 9 trong bảng ASCII
+        {
+            val = val*10;
+            val = val+(chuoi[i]-48);
+            i++;
+        }
+        else if (chuoi[i] == '.')
+        {
+            point = i;
+            printf("dau cham o vi tri %d\n", point);
+            i++    ;    
+        }
+
+    }
+        int imax = i;
+        printf("i cuoi cung %d\n", imax);  
+        //Ý tưởng làm như bên file excel nhưng không được
+
+        // printf("val %d\n", val);
+        // int temp = -(imax - point -1);
+        // printf("-(imax - point -1) %d\n", temp);
+        // double temp2 = 10^temp; 
+        // printf("10^-(imax - point -1) %d\n", temp2);        
+        double valdouble = val*10^(-(imax - point -1));
+    return valdouble;
+    // return val;
+}
+int main(int argc, char const *argv[])
+{
+    char arr[] = "123.4";
+    printf("Ket qua ham atof %d\n", Atof(arr));
+    return 0;
+}
---
+++ BT3_Stdlib/itoa.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int Atoi(char chuoi[])
+{
+    int i = 0;
+    int val = 0;
+    while (chuoi[i] != '\0')
+    {
+        if(chuoi[i]>=48 && chuoi[i]<=57)    //từ 0 đến 9 trong bảng ASCII
+        {
+            val = val*10;
+            val = val+(chuoi[i]-48);
+
+        }
+        i++;
+    }
+    return val;
+}
+int main(int argc, char const *argv[])
+{ 
+    char arr[] = "edfgsd 1234";
+    // Atoi(arr);
+    printf("Ket qua ham itoa %d\n", Atoi(arr));
+    return 0;
+}
